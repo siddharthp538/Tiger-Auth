c003818ec55f3393fa8812c4a6c3b46d44bff70d
varshak333 <varshak333@gmail.com>
2019-02-22 13:08:08 +0530

    mongo error fixed

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const app=express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://sihtigerauth:sihtigerauth2019@ds34765.mlab.com:47665/sihtigerauth' , {useNewUrlParser:true});
-mongoose.connection.once('open', ()=>{
-  console.log('Database Connected..');
+
+const mongoURI = 'mongodb://sihtigerauth:sihtigerauth2019@ds347665.mlab.com:47665/sihtigerauth'
+mongoose.connect(mongoURI,{
+  useNewUrlParser: true 
+})
+.then(() => {
+   console.log('MongoDB connected..');
+})
+.catch(err => {
+   console.log(err);
 });
+
+
 var db = mongoose.connection;
 app.get('/', (req,res)=>{
   res.send({'Hello':'Hi'});
 });
 
-const register = require('./routes/register');
+const register = require('./routes/register/register');
 app.use('/register',register);
 
 
